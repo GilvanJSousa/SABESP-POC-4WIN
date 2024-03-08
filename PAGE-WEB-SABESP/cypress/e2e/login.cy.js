514d76cef1783b0dfa5b958b3fbd982cd272817b
GilvanJSousa <gillvanjs@gmail.com>
2024-03-08 13:38:54 -0300

    teste03

--- PAGE-WEB-SABESP/cypress/e2e/login.cy.js
+++ PAGE-WEB-SABESP/cypress/e2e/login.cy.js
@@ -1,5 +1,15 @@
+require('cypress-xpath');
 describe('Acesso a pagina Sabesp', () => {
   it('Acessar a pagina da Sabesp', () => {
-      cy.visit('https://site.sabesp.com.br/site/Default.aspx')
+      cy.visit('https://agenciavirtual.sabesp.com.br') , // { timeout: 60000}) 
+      cy.xpath('//*[@id="fechaPop"]/span[1]/mat-icon').click()
+      cy.get('.botao-aceitar').click()
+      cy.get('#cpf').type('739.703.398-98')
+      cy.get('#senha').type('@Sabesp4Win')
+      cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()
+      cy.get('iframe[src*=recaptcha]')
+      .its('0.contentDocument')
+      .should(d => d.getElementById('recaptcha-token').click())
+      cy.get('[style="z-index: 2000000000; position: relative; width: 400px; height: 580px;"] > iframe').click()
   })
 })
