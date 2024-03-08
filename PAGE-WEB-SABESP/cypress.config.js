2efddb1451a1aa0a985099760bc9f4cb45d73a1b
GilvanJSousa <gillvanjs@gmail.com>
2024-03-08 10:00:18 -0300

    atualização do GIT pessoal para o corporativo

--- PAGE-WEB-SABESP/cypress.config.js
+++ PAGE-WEB-SABESP/cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    defaultCommandTimeout: 4000,
+    defaultCommandTimeout: 100000,
     pageLoadTimeout: 30000,
     viewportWidth: 1280,
     viewportHeight: 720,
-    baseUrl: "https://sabesp-novaagv-hml.engdb.com.br/",
+    //baseUrl: "https://site.sabesp.com.br/site/Default.aspx",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- PAGE-WEB-SABESP/cypress/e2e/login.cy.js
+++ PAGE-WEB-SABESP/cypress/e2e/login.cy.js
@@ -1,9 +1,5 @@
-describe('Acesso a pagina Sabesp de HOMO', () => {
-  it('Acessar a pagina da Sabesp de Homologação', () => {
-    cy.visit('/')
-    cy.get('#fechaPop > .mat-button-wrapper > .mat-icon').click({force:true})
-    cy.get('.botao-aceitar').click()
-    cy.get('.cpf').type('739.703.398-98')
-    cy.get('.senha').type('@Sabesp4Win')
+describe('Acesso a pagina Sabesp', () => {
+  it('Acessar a pagina da Sabesp', () => {
+      cy.visit('https://site.sabesp.com.br/site/Default.aspx')
   })
 })
